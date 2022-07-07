44bf23c4e4088d6476ea3b35d167b3e097b5d94c
reza <reza138398123@gmail.com>
2022-07-07 18:49:39 +0430

    Header , cookie , Form

--- router/product.py
+++ router/product.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, Header, Cookie, Form
 from fastapi.responses import HTMLResponse, PlainTextResponse
+from typing import List, Optional
 
 
 router = APIRouter(prefix='/product', tags=['product'])
@@ -9,10 +10,13 @@ products = ['watch', 'clock', 'microphone']
 @router.get('/')
 def get_all():
     data = " ".join(products)
-    return Response(content=data, media_type="text/plain")
+    response = Response(content=data, media_type="text/plain")
+    response.headers['my_header'] = 'test header!'
+    response.set_cookie(key="cookie", value="test")
+    return response
 
 
-@router.get('/{id}', responses={
+@router.get('/pro/{id}', responses={
     404: {"content": {"text/plain": {"example": "product not found"}}, "description":"product not found"},
     200: {"content": {"text/html": {"example": "<div> data </div>"}}, "description":"html code data "}
 })
@@ -23,3 +27,14 @@ def get_product(id: int):
         
     data = products[id]
     return HTMLResponse(content=f"<div> {data} </div>", media_type="text/html")
+
+
+@router.get("/withheader")
+def get_products(custom_header: Optional[List[str]] = Header(None), cookie: str = Cookie(None)):
+        return {'data': products, "header": custom_header, "cookie": cookie}
+    
+    
+@router.post('/create')
+def create_product(data: str=Form(...)):
+    products.append(data)
+    return products
